3897520529134717a6948779d93086d5b8271194
vortexwolf <vortexwolf@gmail.com>
2013-08-28 22:02:00 +0300

    Fixed GH-33

--- src/com/vortexwolf/dvach/activities/ExternalUrlHandlerActivity.java
+++ src/com/vortexwolf/dvach/activities/ExternalUrlHandlerActivity.java
@@ -3,6 +3,7 @@ package com.vortexwolf.dvach.activities;
 import com.vortexwolf.dvach.common.utils.UriUtils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,15 +15,16 @@ public class ExternalUrlHandlerActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         Uri uri = this.getIntent().getData();
+        Context context = this.getApplicationContext();
         Intent intent = null;
         if (UriUtils.isBoardUri(uri)) {
-            intent = new Intent(this, ThreadsListActivity.class);
+            intent = new Intent(context, ThreadsListActivity.class);
         } else if (UriUtils.isThreadUri(uri)) {
-            intent = new Intent(this, PostsListActivity.class);
+            intent = new Intent(context, PostsListActivity.class);
         } else if (UriUtils.isImageUri(uri)) {
-            intent = new Intent(this, BrowserActivity.class);
+            intent = new Intent(context, BrowserActivity.class);
         } else {
-            intent = new Intent(this, PickBoardActivity.class);
+            intent = new Intent(context, PickBoardActivity.class);
         }
         
         intent.setData(uri);
@@ -30,5 +32,4 @@ public class ExternalUrlHandlerActivity extends Activity {
         this.startActivity(intent);
         this.finish();
     }
-
 }
