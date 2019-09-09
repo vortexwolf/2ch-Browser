627e8208b5625ec9a655f6309b3e71af788cb86b
Quireg <artur.menchenko@gmail.com>
2019-09-09 11:54:30 +0300

    WebView for captcha is centered in screen now.
    Signed-off-by: Quireg <artur.menchenko@gmail.com>

--- src/com/vortexwolf/chan/activities/NewRecaptchaActivity.java
+++ src/com/vortexwolf/chan/activities/NewRecaptchaActivity.java
@@ -4,25 +4,27 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 import com.vortexwolf.chan.common.library.MyLog;
 
 public class NewRecaptchaActivity extends Activity {
 
-    private final static String data = 
-            "<script src=\"https://www.google.com/recaptcha/api.js\" async defer></script>"+
-            "<form action=\"_intercept\" method=\"GET\">"+
+    private final static String data =
+            "<script src=\"https://www.google.com/recaptcha/api.js\" async defer></script>" +
+                    "<form action=\"_intercept\" method=\"GET\">" +
                     "  <div class=\"g-recaptcha\" data-sitekey=\"6LeQYz4UAAAAAL8JCk35wHSv6cuEV5PyLhI6IxsM\"></div>" +
-            "  <input type=\"submit\" value=\"Submit\">"+
-            "</form>";
+                    "  <input type=\"submit\" value=\"Submit\">" +
+                    "</form>";
 
     private final static String hashfilter = "g-recaptcha-response=";
 
     private static final String DOMAIN = "https://2ch.hk/";
-    
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +33,12 @@ public class NewRecaptchaActivity extends Activity {
         WebView webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
-            @SuppressWarnings("deprecation")
             @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
                 if (url.contains("_intercept")) {
                     if (url.contains(hashfilter)) {
                         String hash = url.substring(url.indexOf(hashfilter) + hashfilter.length());
-                        MyLog.d("INTERCEPTOR", "hash: "+hash);
+                        MyLog.d("INTERCEPTOR", "hash: " + hash);
                         Intent resultIntent = new Intent();
                         resultIntent.putExtra("hash", hash);
                         setResult(RESULT_OK, resultIntent);
@@ -49,8 +50,12 @@ public class NewRecaptchaActivity extends Activity {
                 return super.shouldInterceptRequest(view, url);
             }
         });
+        FrameLayout fm = new FrameLayout(getApplicationContext());
+        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        flp.gravity = Gravity.CENTER;
+        fm.addView(webView, flp);
         webView.loadDataWithBaseURL(DOMAIN, data, "text/html", "UTF-8", null);
-        setContentView(webView);
-
+        setContentView(fm);
     }
 }
